fc82915d574b281e09e6cdd42e2c4f4f5a2c3a90
BeastDev <38675324+BeastDevTM@users.noreply.github.com>
2018-10-02 11:16:21 -0500

    :)

--- example.js
+++ example.js
@@ -1,3 +1,17 @@
+//BEAST JS EXAMPLE:
+
+//STARTS THE INTERVAL:
 Beast.init("gcanvas");
+
+//CREATES THE OBJECT AND GIVES IT COLOR:
 Beast.CO("box1", 0, 0, 200, 200);
 Beast.AA("box1", "COLOR", "blue");
+
+var whattodo = 0;
+while True {
+  if(whattodo === 1) {
+    Beast.AA("box1", "GRAVITY", 1);
+    Beast.RO("box1", "PUSHRIGHT", 10);
+  }
+}
+
